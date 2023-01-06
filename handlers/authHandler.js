67672d2555d97be7f7225d155dd64034c0865329
Hudhayfah Ismail <hoismail2017@gmail.com>
2023-01-06 21:47:03 +0100

    throw

--- handlers/authHandler.js
+++ handlers/authHandler.js
@@ -8,59 +8,103 @@ const { cloudinary } = require('../config/cloudinary');
 
 const auth = {
     register: async (req, userDetails) => {
-        try {
-            let condition = { email: userDetails.email };
-            let options = { lean: true };
+    try {
+        let condition = { email: userDetails.email };
+        let options = { lean: true };
 
-            let exists = await queries.findOne(User, condition, options);
-            console.log(req.file)
-            // console.log(req.file.path)
-            console.log(userDetails.passport)
-            if(!exists) {
-                userDetails.password = factory.generateHashPassword(userDetails.password);
-                // userDetails['passport'] = req.file.originalname;
-                // userDetails['passportPath'] = req.file.path;
-                
-                await cloudinary.uploader.upload(req.file.path, { folder: 'passport' })
-                .then(async (passport) => {
+        let exists = await queries.findOne(User, condition, options);
+        console.log(req.file)
+        console.log(req.file.path)
+        console.log(userDetails.passport)
+        if(!exists) {
+            userDetails.password = factory.generateHashPassword(userDetails.password);
+            // userDetails['passport'] = req.file.originalname;
+            // userDetails['passportPath'] = req.file.path;
+            
+            const passport = await cloudinary.uploader.upload(req.file.path, { folder: 'passport' });
 
-                    userDetails['passport'] = passport.public_id;
-                    userDetails['passportPath'] = passport.secure_url;
+            userDetails['passport'] = passport.public_id;
+            userDetails['passportPath'] = passport.secure_url;
 
-                    console.log(req.file.path)
-    
-                    await queries.create(User, userDetails);
-    
-                    // return {
-                    //     status: 200,
-                    //     data: { msg: 'Check back in afew days to confirm if you\'re eligible to vote or not. Thanks'}
-                    // }
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+            await queries.create(User, userDetails);
 
-                return {
-                    status: 200,
-                    data: { msg: 'Check back in afew days to confirm if you\'re eligible to vote or not. Thanks'}
-                }
-
-            } else {
-
-                // throw new Error('Account already exists');
-                return {
-                    status: 400,
-                    data: { msg: 'Account already exists'}
-                }
+            return {
+                status: 200,
+                data: { msg: 'Check back in afew days to confirm if you\'re eligible to vote or not. Thanks'}
             }
-        } catch (err) {
-            // throw err;
+        } else {
+
+            // throw new Error('Account already exists');
             return {
                 status: 400,
-                data: { msg: err.message }
+                data: { msg: 'Account already exists'}
             }
         }
-    },
+    } catch (err) {
+        throw err;
+        // return {
+        //     status: 400,
+        //     data: { msg: err.message }
+        // }
+    }
+},
+
+    // register: async (req, userDetails) => {
+    //     try {
+    //         let condition = { email: userDetails.email };
+    //         let options = { lean: true };
+
+    //         let exists = await queries.findOne(User, condition, options);
+    //         console.log(req.file)
+    //         // console.log(req.file.path)
+    //         console.log(userDetails.passport)
+    //         if(!exists) {
+    //             userDetails.password = factory.generateHashPassword(userDetails.password);
+    //             // userDetails['passport'] = req.file.originalname;
+    //             // userDetails['passportPath'] = req.file.path;
+                
+    //             await cloudinary.uploader.upload(req.file.path, { folder: 'passport' })
+    //             .then(async (passport) => {
+
+    //                 userDetails['passport'] = passport.public_id;
+    //                 userDetails['passportPath'] = passport.secure_url;
+
+    //                 console.log(userDetails)
+    
+    //                 await queries.create(User, userDetails).then(res => {
+                        
+    //                     return {
+    //                         status: 200,
+    //                         data: { msg: 'Check back in afew days to confirm if you\'re eligible to vote or not. Thanks'}
+    //                     }
+    //                 })
+    
+    //             })
+    //             .catch(err => {
+    //                 console.log(err)
+    //             })
+
+    //             // return {
+    //             //     status: 200,
+    //             //     data: { msg: 'Check back in afew days to confirm if you\'re eligible to vote or not. Thanks'}
+    //             // }
+
+    //         } else {
+
+    //             // throw new Error('Account already exists');
+    //             return {
+    //                 status: 400,
+    //                 data: { msg: 'Account already exists'}
+    //             }
+    //         }
+    //     } catch (err) {
+    //         throw err;
+    //         // return {
+    //         //     status: 400,
+    //         //     data: { msg: err.message }
+    //         // }
+    //     }
+    // },
 
     // register: async (req, userDetails) => {
     //     try {
