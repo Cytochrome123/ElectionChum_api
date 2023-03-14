8785c1f1ef1ee2b08bcc442c52557616d7a2fa61
Hudhayfah <hoismail2017@gmail.com>
2023-03-14 23:22:55 +0100

    fixing cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 // })
 // app.use(cors());
 app.use(cors({
-    // origin: '*',
+    origin: '*',
     // origin: ["http://localhost:3000", 'https://exam-mgt-server.herokuapp.com'], // allow to server to accept request from different origin
     [Access-Control-Allow-Origin]: '*',
     // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
