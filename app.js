edbf2b1a33dd9e36eb65eabc9f012659e442c4e9
Hudhayfah <hoismail2017@gmail.com>
2023-03-14 23:21:12 +0100

    fixing cors

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const db = require('./db');
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // app.use((req, res, next) => {
 //     res.setHeader("Access-Control-Allow-Origin", "*");
 //     res.header(
