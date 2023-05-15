07a29b1754f9359b2040b7a25e4ecd174980eb56
Hudhayfah <hoismail2017@gmail.com>
2023-05-15 20:56:08 +0100

    validation review

--- helper/validation.js
+++ helper/validation.js
@@ -5,12 +5,14 @@ const registerValidation = [
     check('password', 'Passowrd must be'),
     body('passport').custom( ( value, {req} ) => {
         console.log(value)
-        if(req.files.passport[0].mimetype === 'image/jpeg') return true
+        // if(req.files.passport[0].mimetype === 'image/jpeg') return true
+        if(req.files.passport[0].mimetype) return true
         return false;
     }).withMessage('Please upload ur passport'),
     check('Birth Certificate').custom( ( value, {req} ) => {
         console.log(value)
-        if(req.files['Birth Certificate'][0].mimetype === 'image/jpeg' || req.files['Birth Certificate'][0].mimetype === 'image/png') return true
+        // if(req.files['Birth Certificate'][0].mimetype === 'image/jpeg' || req.files['Birth Certificate'][0].mimetype === 'image/png') return true
+        if(req.files['Birth Certificate'][0].mimetype) return true
         return false;
     }).withMessage('Birth Certificate is required'),
 ];
