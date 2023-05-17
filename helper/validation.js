bd4af4a986e9d9455032f126aeb9ab01e29cf3ef
Hudhayfah <hoismail2017@gmail.com>
2023-05-17 17:06:17 +0100

    test 4rm lin

--- helper/validation.js
+++ helper/validation.js
@@ -3,18 +3,18 @@ const { check, body } = require('express-validator');
 const registerValidation = [
     check('email', 'Email is required').not().isEmpty(),
     check('password', 'Passowrd must be'),
-    // body('passport').custom( ( value, {req} ) => {
-    //     console.log(value)
-    //     // if(req.files.passport[0].mimetype === 'image/jpeg') return true
-    //     if(req.files.passport[0].mimetype) return true
-    //     return false;
-    // }).withMessage('Please upload ur passport'),
-    // check('Birth Certificate').custom( ( value, {req} ) => {
-    //     console.log(value)
-    //     // if(req.files['Birth Certificate'][0].mimetype === 'image/jpeg' || req.files['Birth Certificate'][0].mimetype === 'image/png') return true
-    //     if(req.files['Birth Certificate'][0].mimetype) return true
-    //     return false;
-    // }).withMessage('Birth Certificate is required'),
+    body('passport').custom( ( value, {req} ) => {
+        console.log(value)
+        // if(req.files.passport[0].mimetype === 'image/jpeg') return true
+        if(req.files.passport[0].mimetype) return true
+        return false;
+    }).withMessage('Please upload ur passport'),
+    check('Birth Certificate').custom( ( value, {req} ) => {
+        console.log(value)
+        // if(req.files['Birth Certificate'][0].mimetype === 'image/jpeg' || req.files['Birth Certificate'][0].mimetype === 'image/png') return true
+        if(req.files['Birth Certificate'][0].mimetype) return true
+        return false;
+    }).withMessage('Birth Certificate is required'),
 ];
 
 module.exports = registerValidation;
